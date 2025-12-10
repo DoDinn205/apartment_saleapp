bc0a1ba838c4d740e02966069b4444e21db815de
Van Long <tatlong2506@gmail.com>
2025-12-10 18:53:06 +0700

    Thêm trang login, logout nhưng chưa có chức năng

--- eapp/index.py
+++ eapp/index.py
@@ -1,9 +1,47 @@
 from flask import render_template
+from sqlalchemy.sql.operators import endswith_op
+
 from eapp import app
+import os
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', msg='Test123')
+    img_folder = os.path.join(app.static_folder, 'images/banner')
+    images = [
+        'images/banner/' + img for img in os.listdir(img_folder)
+        if img.lower().endswith(('.jpg', '.png', '.jpeg', '.gif'))
+    ]
+
+    room_types=[
+        {
+            'id': 1,
+            'name': '1 phòng ngủ'
+        },
+        {
+            'id': 2,
+            'name': '2 phòng ngủ'
+        },
+        {
+            'id': 3,
+            'name': '3 phòng ngủ'
+        },
+        {
+            'id': 4,
+            'name': '4 phòng ngủ'
+        }
+    ]
+    return render_template('index.html', images=images, room_types=room_types)
+
+
+@app.route('/login')
+def login_view():
+    return render_template('login.html')
+
+@app.route('/register')
+def register_view():
+    return render_template('register.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)

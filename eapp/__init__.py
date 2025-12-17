305638aac955557ac8528bc29fe2f5b465319002
2351010048dien@ou.edu.vn <2351010048dien@ou.edu.vn>
2025-12-18 02:49:33 +0700

    Chỉnh sửa lại giao diện Admin, tích hợp cloudinary, thêm chức năng upload ảnh căn hộ lên cloudinary

--- eapp/__init__.py
+++ eapp/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import cloudinary
 
 app = Flask(__name__)
 app.secret_key = 'BiMat123'
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:tatvanlong123@localhost/apartmentdb?charset=utf8mb4"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:12345678@localhost/apartmentdb?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app=app)
 
 login = LoginManager(app=app)
+
+cloudinary.config(
+    cloud_name="dzlfmtf2q",
+    api_key="394863965996431",
+    api_secret="3ZOOFpQffZyxR2gmN_3e_zDSrnM"
+)
